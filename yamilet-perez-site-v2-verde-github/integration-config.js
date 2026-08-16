681f661f1dffb3413ac31c46036e89ff17497c7c
Rujumagil <proyectocompas.info@gmail.com>
2026-08-16 09:01:52 -0600

    feat: add handoff integration configuration

---
+++ yamilet-perez-site-v2-verde-github/integration-config.js
@@ -0,0 +1,12 @@
+window.YAMILET_INTEGRATION = Object.freeze({
+  workspaceSlug: "yamilet-mes",
+  localeDefault: "es",
+  academy: {
+    enabled: false,
+    url: "https://academia.yamiletperez.com"
+  },
+  leadCapture: {
+    enabled: false,
+    endpoint: ""
+  }
+});
